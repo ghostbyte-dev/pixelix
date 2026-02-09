dfa89271cad2d6100f11d2afb76149e4bec78eba
Hiebeler <emanuel.hiebeler@gmail.com>
2026-02-09 16:34:03 +0100

    #127 make accountSettings fields nullable

--- app/src/commonMain/kotlin/com/daniebeler/pfpixelix/domain/model/Settings.kt
+++ app/src/commonMain/kotlin/com/daniebeler/pfpixelix/domain/model/Settings.kt
@@ -6,7 +6,7 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class Settings(
     @SerialName("enable_reblogs") val enableReblogs: Boolean,
-    @SerialName("hide_collections") val hideCollections: Boolean,
-    @SerialName("hide_groups") val hideGroups: Boolean,
-    @SerialName("hide_stories") val hideStories: Boolean,
+    @SerialName("hide_collections") val hideCollections: Boolean?,
+    @SerialName("hide_groups") val hideGroups: Boolean?,
+    @SerialName("hide_stories") val hideStories: Boolean?,
 )

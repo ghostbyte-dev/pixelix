ad75586e56a3d799ef7431c36503575ee9510523
Hiebeler <emanuel.hiebeler@gmail.com>
2026-09-04 22:21:15 +0200

    use unified push as default, fcm only if no unified push existing

--- sharedUI/src/androidMain/kotlin/com/daniebeler/pfpixelix/utils/PushNotifications.android.kt
+++ sharedUI/src/androidMain/kotlin/com/daniebeler/pfpixelix/utils/PushNotifications.android.kt
@@ -10,13 +10,23 @@ actual fun initializePushNotifications(context: Any?, activeUser: String) {
     val keyManager = DefaultKeyManager(ctx)
 
     val distributors = UnifiedPush.getDistributors(ctx)
-    if (distributors.isNotEmpty()) {
+    if (distributors.isEmpty()) {
+        return
+    } else if (distributors.size == 1) {
         Logger.d("pushNotification") {
-            "distributors found: " + distributors.size
+            "distributors found: 1, fcm"
         }
+
         UnifiedPush.saveDistributor(ctx, distributors.first())
         UnifiedPush.register(ctx, activeUser, keyManager = keyManager)
     } else {
-        Logger.d("pushNotification") { "no distributor found" }
+        Logger.d("pushNotification") {
+            "distributors found: " + distributors.size
+        }
+        Logger.d("pushNotification") {
+            "distributors: $distributors"
+        }
+        UnifiedPush.saveDistributor(ctx, distributors[1])
+        UnifiedPush.register(ctx, activeUser, keyManager = keyManager)
     }
 }

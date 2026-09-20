ff40463c803aff5ae3332977037b78b138b99cb4
Hiebeler <emanuel.hiebeler@gmail.com>
2026-09-20 22:42:20 +0200

    give new variable acct in credentials default value

--- sharedUI/src/commonMain/kotlin/com/daniebeler/pfpixelix/domain/model/Credentials.kt
+++ sharedUI/src/commonMain/kotlin/com/daniebeler/pfpixelix/domain/model/Credentials.kt
@@ -7,7 +7,7 @@ import kotlinx.serialization.Serializable
 data class Credentials(
     val accountId: String,
     val username: String,
-    val acct: String?,
+    val acct: String? = null,
     val displayName: String,
     val avatar: String,
     val serverUrl: String,
